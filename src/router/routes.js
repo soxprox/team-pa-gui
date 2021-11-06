18675f7946788ebcad88e0c2e691c2b42a0fb684
simon <simonjcarr@gmail.com>
2021-11-06 10:22:11 +0000

    added route to dashboard

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,16 @@ const routes = [
     path: '/',
     component: () => import('layouts/SplashLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/SplashPage.vue') }
     ]
   },
-
+  {
+    path: '/app/dashboard',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/DashboardPage.vue') }
+    ]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
