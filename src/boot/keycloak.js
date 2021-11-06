9803ab2f6750ad8356cc6cbabe296c7ac384e6b9
simon <simonjcarr@gmail.com>
2021-11-06 20:49:00 +0000

    Return promise from keycloak boot file

--- src/boot/keycloak.js
+++ src/boot/keycloak.js
@@ -1,19 +1,22 @@
 import Keycloak from "keycloak-js";
 
 export default ({ store }) => {
-  const initOptions = {
-    url: process.env.KEYCLOAK_URL,
-    realm: process.env.KEYCLOAK_REALM,
-    clientId: process.env.KEYCLOAK_CLIENT_ID,
-  };
+  return new Promise((resolve, reject) => {
+    const initOptions = {
+      url: process.env.KEYCLOAK_URL,
+      realm: process.env.KEYCLOAK_REALM,
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+    };
 
-  let keycloak = Keycloak(initOptions);
-  keycloak
-    .init({ onLoad: "check-sso" })
-    .then(() => {
-      store.dispatch("keycloak/setKeycloak", keycloak);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    let keycloak = Keycloak(initOptions);
+    keycloak
+      .init({ onLoad: "check-sso" })
+      .then(() => {
+        store.dispatch("keycloak/setKeycloak", keycloak);
+        resolve();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  })
 };
