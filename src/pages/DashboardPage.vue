d5139a6ad87e895238f5a9eaa2a84126a727b8e0
simon <simonjcarr@gmail.com>
2021-11-06 10:20:29 +0000

    added dashboard

---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page padding>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <MeetingList/>
+      <MeetingList/>
+      <MeetingList/>
+      <MeetingList/>
+      <MeetingList/>
+      <MeetingList/>
+      <MeetingList/>
+      <MeetingList/>
+      <MeetingList/>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import MeetingList from '../components/meetings/MeetingList.vue'
+export default {
+  components: {
+    MeetingList
+  }
+}
+</script>
+
+<style>
+
+</style>
